addcd7d3188b2fc5cf72e3a96924ed98914cb71c
Talesr323 <talesr323@gmail.com>
2023-05-30 22:52:17 -0300

    mais att ate agr

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { Routes,Route } from "react-router-dom";
 import { Repos } from "./pages/Repos";
-import { Repo } from "./pages/Repo";
 
 export function App() {
   return(
     <Routes>
       <Route path="/" element={<Repos />} />
-      <Route path="/repos/*" element={<Repo />} />
+      <Route path="/repos/*" element={<Repos />} />
     </Routes>
   )
 }
--- src/pages/Repo.tsx
+++
@@ -1,3 +0,0 @@
-export function Repo() {
-    return <h1> HELLO</h1>
-}
