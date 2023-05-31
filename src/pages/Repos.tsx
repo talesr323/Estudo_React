8f4b2ab5f134304c44a5da77df53ebbe924beec4
Talesr323 <talesr323@gmail.com>
2023-05-31 10:42:22 -0300

    Codigo Atualizado

--- src/pages/Repos.tsx
+++ src/pages/Repos.tsx
@@ -1,33 +1,59 @@
 import axios from 'axios';
-import { useQuery } from 'react-query'
+import { useQuery } from 'react-query';
+import "./style.css"
 
 type Repository = {
-  id: string;
-  title: string;
-  owner: string;
+  codigoVenda: string;
+  dataVencimento: string;
+  NumeroBoleto:string;
+  NumeroDocumento:string;
+  cliente: string;
 }
 
 export function Repos() {
   const { data, isFetching } = useQuery<Repository[]>('/projects', async () => {
-
     const response = await axios.get('http://localhost:3033/projects')
-
     return response.data;
   })
 
   return (
-    <ul>
+    <div className='Container'>
       {isFetching && <p>Carregando...</p>}
-      {data?.map(repo => {
-        return(
-        <li key={repo.id}>
-          <h1>{repo.id}</h1>
-          <strong>{repo.title}</strong>
-          <p>{repo.owner}</p>
-        </li>
-        )
-      })}
-    </ul>
+      <table className='Relatorios'>
+        <caption>Relatorios</caption>
+        <thead>
+          <tr>
+            <th>Venda Codigo</th>
+            <th>Data Vencimento</th>
+            <th>Numero do Boleto</th>
+            <th>Numero do Documento</th>
+            <th>Cliente Nome Fantasia</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.map(repo => (
+            <tr key={repo.codigoVenda}>
+              <td>{repo.codigoVenda}</td>
+              <td>{repo.dataVencimento}</td>
+              <td>{repo.NumeroBoleto}</td>
+              <td>{repo.NumeroDocumento}</td>
+              <td>{repo.cliente}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
+
+/**
+ * CodVenda    ---  codigoVenda
+ * DataFaturamento   ---
+ * DataVenda   ---
+ * NumBoleto   ---  NumeroBoleto
+ * NumDocumento   ---   NumeroDocumento
+ * CliNomeFantasia   ---  Cliente
+ * FormaPagamento   ---
+ * ValorEntrada   ---
+*/
